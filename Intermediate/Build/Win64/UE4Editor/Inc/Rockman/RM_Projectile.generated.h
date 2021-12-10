45f803c342df79e7e4fe77b2957a6533ee91f58c
semocookie <wjrm700@naver.com>
2021-12-10 18:14:16 +0900

    Enemy and Enemy AI(Chase or Patrol)

--- Intermediate/Build/Win64/UE4Editor/Inc/Rockman/RM_Projectile.generated.h
+++ Intermediate/Build/Win64/UE4Editor/Inc/Rockman/RM_Projectile.generated.h
@@ -15,20 +15,20 @@ struct FRotator;
 #endif
 #define ROCKMAN_RM_Projectile_generated_h
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_SPARSE_DATA
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_SPARSE_DATA
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS \
  \
 	DECLARE_FUNCTION(execSetActive); \
 	DECLARE_FUNCTION(execProjectileSetLocation);
 
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
  \
 	DECLARE_FUNCTION(execSetActive); \
 	DECLARE_FUNCTION(execProjectileSetLocation);
 
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_INCLASS_NO_PURE_DECLS \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesARM_Projectile(); \
 	friend struct Z_Construct_UClass_ARM_Projectile_Statics; \
@@ -37,7 +37,7 @@ public: \
 	DECLARE_SERIALIZER(ARM_Projectile)
 
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_INCLASS \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_INCLASS \
 private: \
 	static void StaticRegisterNativesARM_Projectile(); \
 	friend struct Z_Construct_UClass_ARM_Projectile_Statics; \
@@ -46,7 +46,7 @@ public: \
 	DECLARE_SERIALIZER(ARM_Projectile)
 
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_STANDARD_CONSTRUCTORS \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API ARM_Projectile(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARM_Projectile) \
@@ -59,7 +59,7 @@ private: \
 public:
 
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_ENHANCED_CONSTRUCTORS \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API ARM_Projectile(ARM_Projectile&&); \
@@ -70,28 +70,28 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARM_Projectile); \
 	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ARM_Projectile)
 
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_PRIVATE_PROPERTY_OFFSET
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_11_PROLOG
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_GENERATED_BODY_LEGACY \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_PRIVATE_PROPERTY_OFFSET
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_11_PROLOG
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_PRIVATE_PROPERTY_OFFSET \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_SPARSE_DATA \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_INCLASS \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_STANDARD_CONSTRUCTORS \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_PRIVATE_PROPERTY_OFFSET \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_SPARSE_DATA \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_INCLASS \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define Rockman6_12_Source_Rockman_RM_Projectile_h_14_GENERATED_BODY \
+#define Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_PRIVATE_PROPERTY_OFFSET \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_SPARSE_DATA \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_INCLASS_NO_PURE_DECLS \
-	Rockman6_12_Source_Rockman_RM_Projectile_h_14_ENHANCED_CONSTRUCTORS \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_PRIVATE_PROPERTY_OFFSET \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_SPARSE_DATA \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_INCLASS_NO_PURE_DECLS \
+	Rockman6_12__2__Source_Rockman_RM_Projectile_h_14_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -99,7 +99,7 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 template<> ROCKMAN_API UClass* StaticClass<class ARM_Projectile>();
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID Rockman6_12_Source_Rockman_RM_Projectile_h
+#define CURRENT_FILE_ID Rockman6_12__2__Source_Rockman_RM_Projectile_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
